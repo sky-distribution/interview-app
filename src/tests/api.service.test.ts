a3edc29c4405c7c53948bcb4029e0b85ab07d3f5
Jan Kowalski <jkowalski@skydistribution.pl>
2020-11-18 11:07:24 +0100

    useless test

--- src/tests/api.service.test.ts
+++ src/tests/api.service.test.ts
@@ -8,4 +8,10 @@ describe('apiService', () => {
     //then
     expect(result).toHaveLength(3)
   })
+
+  const sum = (a: number, b: number) => a + b
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  
 })
